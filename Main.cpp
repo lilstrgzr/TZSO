7355fe19e3cb57e7e688f889a5037f4cbf9556d2
Kirill Liapustin <asaprokki@gmail.com>
2024-11-05 21:16:02 +0500

    1.1

--- Main.cpp
+++ Main.cpp
@@ -28,24 +28,38 @@ public:
 		return number;
 	}
 
-	void print_workers(){
-		std::cout << name << " " << surname << " " << number << "\n\n";
+	bool operator<(const Worker& other) const {
+		if (surname != other.surname)
+			return surname < other.surname;
+		if (name != name)
+			return name < other.name;
+		return number < other.number;
+	}
+	
+	void print_workers() const {
+		std::cout << name << " " << surname << " " << number << "\n";
 	};
 };
+
+bool Names(const Worker& p1, const Worker& p2) {
+	return p1.getName() < p2.getName();
+}
+
+
+bool Surnames(const Worker& p1, const Worker& p2) {
+	return p1.getSurname() < p2.getSurname();
+}
+
+bool Numbers(const Worker& p1, const Worker& p2) {
+	return p1.getNumber() < p2.getNumber();
+}
 
 int main() {
 	setlocale(LC_ALL, "russian");
 
-	Worker W1("Иван", "Иванов", 3515454);
-	Worker W2("Иван", "Сидоров", 3515422);
-	Worker W3("Олег", "Сидоров", 23515423);
-	Worker W4("Иван","Петров", 3515455);
 
-	std::vector<std::string> names{W1.getName(), W2.getName(), W3.getName(), W4.getName(), };
-	std::vector<std::string> surnames{ W1.getSurname(),W2.getSurname(), W3.getSurname(), W4.getSurname(), };
-	std::vector<unsigned> numbers{ W1.getNumber(),W2.getNumber(), W3.getNumber(), W4.getNumber(), };
+	std::vector<Worker>managers{Worker("Иван", "Иванов", 3515454), Worker("Иван", "Сидоров", 3515422), Worker("Олег", "Сидоров", 23515423), Worker("Иван","Петров", 3515455),};
 
-	W4.print_workers();
 
 	bool i = false;
 
@@ -58,23 +72,23 @@ int main() {
 		std::cin >> choice;
 		switch (choice) {
 		case 1: {
-			std::sort(begin(names), end(names));
-			for (auto& names: names) {
-				std::cout << names << std::endl;
+			std::sort(managers.begin(), managers.end(), Names);
+			for (const auto& Worker : managers) {
+				Worker.print_workers();
 			}
 			break;
 			}
 		case 2: {
-			std::sort(begin(surnames), end(surnames));
-			for (auto& surnames : surnames) {
-				std::cout << surnames << std::endl;
+			std::sort(managers.begin(), managers.end(), Surnames);
+			for (const auto& Worker : managers) {
+				Worker.print_workers();
 			}
 			break;
 			}
 		case 3:{
-			std::sort(begin(numbers), end(numbers));
-			for (auto& numbers : numbers) {
-				std::cout << numbers << std::endl;
+			std::sort(managers.begin(), managers.end(), Numbers);
+			for (const auto& Worker : managers) {
+				Worker.print_workers();
 			}
 			break;
 			}
